613cf00195d36be06ea211673253592df50ee6ad
Kevin Ashcraft <kevin@kevashcraft.com>
2019-07-13 22:51:01 -0400

    solidsend

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="app">
-    <Radiogram :cc="callsign" @send="addRadiogram($event)" />
+    <Radiogram @send="confirmRadiogram($event)" ref="radiogram" />
+    <md-dialog :md-active.sync="showRadiogramConfirm" class="radiogram-confirm-dialog">
+      <md-dialog-title>Confirm Radiogram Transmission</md-dialog-title>
+      <md-content>
+        <div class="radiogram-preview-header">
+          <p>
+            <span>{{radiogram.number}}</span>
+            <span>-</span>
+            <span>{{radiogram.precedence}}</span>
+            <span>-</span>
+            <span>{{radiogram.hx}}</span>
+            <span>-</span>
+            <span>{{radiogram.stationOfOrigin}}</span>
+            <span>-</span>
+            <span>{{radiogram.check}}</span>
+            <span>-</span>
+            <span v-show="radiogram.placeOfOrigin">{{radiogram.placeOfOrigin}}</span>
+            <span v-show="radiogram.placeOfOrigin">-</span>
+            <span>{{radiogram.timeFiled}}</span>
+            <span>-</span>
+            <span>{{radiogram.dateFiled}}</span>
+          </p>
+        </div>
+        <div class="radiogram-preview-text">
+          <p>{{radiogram.message}}</p>
+        </div>
+      </md-content>
+      <md-dialog-actions>
+        <md-button @click="addRadiogram" class="md-primary md-raised">Send it</md-button>
+      </md-dialog-actions>
+    </md-dialog>
     <CallsignDialog ref="callsignDialog" @callsign="setCallsign" />
     <md-button @click="openCallsignDialog" class="md-primary md-raised">Open</md-button>
     <md-list>
@@ -8,7 +38,9 @@
         <span>{{msg.number}}</span>
       </md-list-item>
     </md-list>
-  </div>
+    <md-snackbar md-position="center" :md-duration="2500" :md-active.sync="showSnackbar" md-persistent>
+      <span>{{ snackbarMessage }}</span>
+    </md-snackbar>  </div>
 </template>
 
 <script>
@@ -16,6 +48,7 @@ import CallsignDialog from './components/CallsignDialog.vue'
 import Radiogram from './components/Radiogram.vue'
 import firebase from 'firebase'
 import 'firebase/firestore'
+import moment from 'moment'
 
 function makeid(length) {
   let result = '';
@@ -58,7 +91,11 @@ export default {
       callsignDialogOpened: false,
       snapshotListener: false,
       messages: [],
-      count: 0
+      count: 0,
+      showRadiogramConfirm: false,
+      showSnackbar: false,
+      snackbarMessage: '',
+      radiogram: {}
     }
   },
   mounted () {
@@ -73,34 +110,115 @@ export default {
       this.getMessages()
     },
     getMessages () {
-      let ref = this.db.collection('testing').doc(this.callsign).collection('2019')
-      ref.get().then(snapshot => {
-        let messages = []
-        snapshot.forEach(doc => messages.push(doc.data()))
-        this.messages = messages
-      })
+      console.log('master send first');
+      // let ref = this.db.collection('testing').doc(this.callsign).collection('2019')
+      // ref.get().then(snapshot => {
+      //   let messages = []
+      //   snapshot.forEach(doc => messages.push(doc.data()))
+      //   this.messages = messages
+      // })
     },
-    addRadiogram(radiogram) {
-      let docId = makeid(7)
-      let stationRef =  this.db.collection('testing').doc(this.callsign)
-      let docRef = stationRef.collection('2019').doc(docId)
+    confirmRadiogram(radiogram) {
+      this.radiogram = this.formatRadiogram(radiogram)
+      let snackbar = false
+
+      // ensure number is int
+      if (!Number.isInteger(this.radiogram.number) || !(this.radiogram.number > 0)) {
+        snackbar = 'Error: Number must be an integer'
+        this.$refs['radiogram'].focusInput('number')
+      } else if (typeof this.radiogram.precedence !== 'string' || this.radiogram.precedence.length > 8) {
+        snackbar = 'Error: Precedence too long'
+        this.$refs['radiogram'].focusInput('precedence')
+      } else if (typeof this.radiogram.hx !== 'string' || this.radiogram.hx.length > 8) {
+        snackbar = 'Error: HX too long'
+        this.$refs['radiogram'].focusInput('hx')
+      } else if (typeof this.radiogram.stationOfOrigin !== 'string' || this.radiogram.stationOfOrigin.length < 4 || this.radiogram.stationOfOrigin.length > 16) {
+        snackbar = 'Error: Station of Origin'
+        this.$refs['radiogram'].focusInput('stationOfOrigin')
+      } else if (typeof this.radiogram.placeOfOrigin !== 'string' || this.radiogram.placeOfOrigin.length > 32) {
+        snackbar = 'Error: Station of Origin'
+        this.$refs['radiogram'].focusInput('placeOfOrigin')
+      } else if (typeof this.radiogram.dateFiled !== 'string' || this.radiogram.dateFiled.length !== 10) {
+        snackbar = 'Error: Date is wrong'
+        this.$refs['radiogram'].focusInput('dateFiled')
+      } else if (typeof this.radiogram.timeFiled !== 'string' || this.radiogram.timeFiled.length !== 5) {
+        snackbar = 'Error: Time is wrong'
+        this.$refs['radiogram'].focusInput('timeFiled')
+      } else if (typeof this.radiogram.destination !== 'string' || this.radiogram.destination.length < 4 || this.radiogram.destination.length > 2056) {
+        snackbar = 'Error: Invalid destination'
+        this.$refs['radiogram'].focusInput('destination')
+      } else if (typeof this.radiogram.destinationPhone !== 'string' || this.radiogram.destinationPhone.length > 64) {
+        snackbar = 'Error: Invalid destination phone'
+        this.$refs['radiogram'].focusInput('destinationPhone')
+      } else if (typeof this.radiogram.destinationEmail !== 'string' || this.radiogram.destinationEmail.length > 128) {
+        snackbar = 'Error: Invalid destination email'
+        this.$refs['radiogram'].focusInput('destinationEmail')
+      } else if (!Number.isInteger(this.radiogram.check) || this.radiogram.check > 25) {
+        snackbar = 'Error: Message too long'
+      } else if (typeof this.radiogram.message !== 'string' || this.radiogram.message.length > 2056) {
+        snackbar = 'Error: Message too long'
+      } else if (this.radiogram.message.length < 5) {
+        snackbar = 'Error: Message not long enough'
+      }
+
+      if (snackbar === false) {
+        this.showRadiogramConfirm = true
+      } else {
+        this.snackbarMessage = snackbar
+        this.showSnackbar = true
+      }
+
+    },
+    addRadiogram() {
+      let radiogram = Object.assign({}, this.radiogram)
+      radiogram.timestamp = firebase.firestore.FieldValue.serverTimestamp()
+      radiogram.message = radiogram.messageGroups.join(' ')
+
+      let docId = moment().format('YYYY') + '-' + radiogram.number + '-' + makeid(4)
+
+      let stationRef =  this.db.collection('callsigns').doc('KM4FPA')
+      let docRef = stationRef.collection('messages').doc(docId)
       docRef.set(radiogram).then(() => {
-        this.getMessages()
+        this.showRadiogramConfirm = false
+        this.snackbarMessage = 'Message sent!'
+        this.showSnackbar = true;
+        // this.getMessages()
       })
       .catch((error) => {
-          console.error("Error writing document: ", error);
+        this.snackbarMessage = 'Error: ' + error
+        this.showSnackbar = true
       });
-    }
+    },
+    formatRadiogram(data) {
+      let radiogram = Object.assign({}, data)
+      radiogram.check = radiogram.messageGroups.length
+      radiogram.message = radiogram.messageGroups.map(m => m.txt).join(' ')
+      return radiogram
+
+    },
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.radiogram-preview-header {
+  span {
+    margin: 0 5px;
+  }
+}
+.radiogram-preview-message {
+  margin: 15px;
+}
+.radiogram-confirm-dialog {
+  max-width: 350px !important;
+  max-height: 400px !important;
 }
 </style>
--- app/src/components/Radiogram.vue
+++ app/src/components/Radiogram.vue
@@ -1,111 +1,221 @@
 <template>
-  <md-content class="radiogram">
-    <form class="md-layout" @submit.prevent="send">
-      <div class="form-row">
-        <md-field>
-          <label>Number</label>
-          <md-input v-model="radiogram.number" type="number"></md-input>
-        </md-field>
-        <md-field>
-          <label>Precedence</label>
-          <md-input v-model="radiogram.precedence" ></md-input>
-        </md-field>
-        <md-field>
-          <label>[HX]</label>
-          <md-input v-model="radiogram.hx" ></md-input>
-        </md-field>
-        <md-field>
-          <label>Station of Origin</label>
-          <md-input v-model="callsign" disabled></md-input>
-        </md-field>
-        <md-field>
-          <label>Check</label>
-          <md-input :value="check" disabled></md-input>
-        </md-field>
-        <md-field>
-          <label>Place of Origin</label>
-          <md-input v-model="radiogram.placeOfOrigin" disabled></md-input>
-        </md-field>
-        <md-field>
-          <label>Time Filed</label>
-          <md-input v-model="radiogram.time" type="time"></md-input>
-        </md-field>
-        <md-field>
-          <label>Date Filed</label>
-          <md-input v-model="radiogram.date" type="date"></md-input>
-        </md-field>
-      </div>
-      <div class="form-row">
-        <md-field>
-          <label>Station, Address</label>
-          <md-input v-model="radiogram.toStation"></md-input>
-        </md-field>
-        <md-field>
-          <label>Phone</label>
-          <md-input v-model="radiogram.toPhone" type="phone"></md-input>
-        </md-field>
-        <md-field>
-          <label>Email</label>
-          <md-input v-model="radiogram.toEmail" type="email"></md-input>
-        </md-field>
-      </div>
-      <div class="form-row">
-        <md-chips v-model="radiogram.message" md-placeholder="Message" ref="chips"></md-chips>
-      </div>
-      <div class="form-row">
-        <md-button type="submit">Send</md-button>
-      </div>
-    </form>
-  </md-content>
+  <md-card class="radiogram">
+    <md-card-header>
+      <h1>RADIOGRAM</h1>
+    </md-card-header>
+    <md-card-content>
+      <form class="md-layout radiogram-form" @submit.prevent="send" ref="form">
+        <div class="form-row">
+          <md-field class="rg-col col-s1">
+            <label>Number</label>
+            <md-input v-model="radiogram.number" type="number" ref="number"></md-input>
+          </md-field>
+          <md-field class="rg-col col-s1">
+            <label>Precedence</label>
+            <md-input v-model="radiogram.precedence" ref="precedence"></md-input>
+          </md-field>
+          <md-field class="rg-col col-s1">
+            <label>[HX]</label>
+            <md-input v-model="radiogram.hx"  ref="hx"></md-input>
+          </md-field>
+          <md-field class="rg-col col-s2">
+            <label>Station of Origin</label>
+            <md-input v-model="radiogram.stationOfOrigin" disabled ref="stationOfOrigin"></md-input>
+          </md-field>
+          <md-field class="rg-col col-s1">
+            <label>Check</label>
+            <md-input :value="check" disabled></md-input>
+          </md-field>
+          <md-field class="rg-col col-s2">
+            <label>Place of Origin</label>
+            <md-input v-model="radiogram.placeOfOrigin" disabled ref="placeOfOrigin"></md-input>
+          </md-field>
+          <md-field class="rg-col col-s2">
+            <label>Time Filed</label>
+            <md-input v-model="radiogram.timeFiled" type="time" ref="timeFiled"></md-input>
+          </md-field>
+          <md-field class="rg-col col-s2">
+            <label>Date Filed</label>
+            <md-input v-model="radiogram.dateFiled" type="date" ref="dateFiled"></md-input>
+          </md-field>
+        </div>
+        <div class="form-row">
+          <md-field class="rg-col col-s3 no-flex">
+            <label class="bold-label">To</label>
+            <md-textarea v-model="radiogram.destination" ref="destination"></md-textarea>
+          </md-field>
+          <div class="form-col">
+            <md-field class="rg-col col-s2">
+              <label>Phone</label>
+              <md-input v-model="radiogram.destinationPhone" type="phone" ref="destinationPhone"></md-input>
+            </md-field>
+            <md-field class="rg-col col-s2">
+              <label>Email</label>
+              <md-input v-model="radiogram.destinationEmail" type="email" ref="destinationEmail"></md-input>
+            </md-field>
+          </div>
+          <div style="flex: 1"></div>
+        </div>
+        <div class="form-row flex-start">
+          <md-field v-for="(grp, idx) in radiogram.messageGroups" :key="idx" class="rg-col msg-col">
+            <label>{{idx+1}}</label>
+            <md-input v-model="grp.txt" @input.native="checkForSpace($event, idx)" :ref="inputRef(idx)"></md-input>
+          </md-field>
+        </div>
+        <div class="form-row">
+          <md-button type="submit">Send</md-button>
+        </div>
+      </form>
+    </md-card-content>
+  </md-card>
 </template>
 
 <style lang="scss" scoped>
 .form-row {
   display: flex;
-  border: 1px solid orange;
+  flex-wrap: wrap;
+  justify-content: stretch;
+  margin: 10px 0;
+  .md-field {
+    flex: 1;
+  }
+  &.flex-start {
+    justify-content: space-around;
+  }
 }
+.form-col {
+  // display: flex;
+  // flex-direction: column;
+  // justify-content: flex-start;
+  // align-items: flex-start;
+  width: 300px;
+  flex: 0;
+}
+.radiogram-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+// .rg-col col-s1 {
+//   width: 100px;
+// }
+.rg-col {
+  label {
+    top: 0 !important;
+    font-size: 12px;
+  }
+}
+.msg-col {
+  flex: 0 !important;
+  input {
+    max-width: 150px;
+  }
+  label {
+    opacity: .4;
+  }
+  @media screen and (max-width: 800px) {
+    flex: 1 !important;
+    min-width: 25% !important;
+    max-width: 30% !important;
+  }
+}
+.col-s1 {
+  width: 100px;
+  input {
+    max-width: 65px;
+  }
+}
+.col-s15 {
+  width: 150px;
+  input {
+    max-width: 95px;
+  }
+}
+.col-s2 {
+  width: 180px;
+  input {
+    max-width: 130px;
+  }
+}
+.col-s3 {
+  width: 300px;
+  input {
+    max-width: 250px;
+  }
+  textarea {
+    width: 300px;
+    max-width: 300px;
+  }
+  margin-right: 15px;
+}
+.msg-col {
+  margin: 15px;
+}
+.bold-label {
+  font-weight: bold;
+}
+.no-flex {
+  flex: 0 !important;
+}
+.md-field {
+  margin-bottom: 0 !important;
+}
+
 </style>
 
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'Radiogram',
-  props: {
-    cs: String,
-  },
   computed: {
     check () {
-      return this.radiogram.message.length
+      return this.radiogram.messageGroups.filter(g => g.txt.length > 0).length
     },
-  },
-  watch: {
-    cs (callsign) {
-      this.callsign = callsign
-    }
   },
   data () {
     return {
       radiogram: {
-        message: []
+        number: 1,
+        precedence: 'R',
+        hx: 'HXG',
+        stationOfOrigin: 'KM4FPA',
+        placeOfOrigin: '',
+        dateFiled: moment().format('YYYY-MM-DD'),
+        timeFiled: moment().format('HH:mm'),
+        destination: 'tdemop',
+        destinationEmail: '',
+        destinationPhone: '',
+        messageGroups: new Array(25).fill().map(() => Object.assign({}, {txt: ''}))
       },
-      callsign: ''
     }
   },
   mounted () {
-    let inp = this.$refs['chips'].$refs['input'].$el
-    this.chipInput = inp
-    inp.addEventListener('keyup', event => {
-      if (event.keyCode === 32) {
-        this.addChip()
-      }
-    })
   },
   methods: {
-    addChip () {
-      let chip = this.$refs['chips']._data.inputValue
-      this.radiogram.message.push(chip)
-
-      this.$refs['chips']._data.inputValue = ''
+    inputRef (idx) {
+      return `input-${idx}`
+    },
+    checkForSpace(e, idx) {
+      // console.log('e', e);
+      // console.log('e', e);
+      // this.radiogram.destination = JSON.stringify(e)
+      // this.radiogram.destinationEmail += e.charCode
+      if (e.data && e.data === ' ') {
+        if (idx < this.radiogram.messageGroups.length - 1) {
+          this.$refs[`input-${idx+1}`][0].$el.focus()
+        }
+      }
+    },
+    focusInput (input) {
+      this.$refs['form'].scrollIntoView()
+      this.$refs[input].$el.focus()
+    },
+    preventSpace (event) {
+      if (event.keyCode && event.keyCode === 32) {
+        event.preventDefault()
+      }
     },
     send () {
       this.$emit('send', this.radiogram)
